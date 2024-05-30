2e479792d5477562aa0ad34ee1440e17e044aba5
Felix Hillebrand <astraljaeger@pm.me>
2024-05-30 16:25:53 +0200

    add logs to index.ts add qonda for local runs

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
+import * as core from '@actions/core'
 import { run } from './main'
 
 run()
+  .then(() => {
+    core.info('Action complete')
+  })
+  .catch(error => {
+    core.setFailed(error.message)
+  })
